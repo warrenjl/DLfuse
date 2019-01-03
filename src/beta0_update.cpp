9655798f4a6b7fdfe0996703517e63567b1e516d
Warren <joshua.warren@yale.edu>
2019-01-02 19:02:28 -0500

    Initial commit.

---
+++ src/beta0_update.cpp
@@ -0,0 +1,27 @@
+#include "RcppArmadillo.h"
+#include "DLfuse.h"
+using namespace arma;
+using namespace Rcpp;
+
+// [[Rcpp::depends(RcppArmadillo)]]
+// [[Rcpp::export]]
+
+double beta0_update(arma::vec y,
+                    arma::vec sample_size,
+                    double sigma2_beta,
+                    arma::vec mean_temp, 
+                    double sigma2_epsilon){
+  
+double beta0_var = (sigma2_beta*sigma2_epsilon)/(sum(sample_size)*sigma2_beta + sigma2_epsilon);
+  
+double beta0_mean = sigma2_beta*sum(y - mean_temp)/(sum(sample_size)*sigma2_beta + sigma2_epsilon);
+
+double beta0 = R::rnorm(beta0_mean,
+                        sqrt(beta0_var));
+  
+return(beta0);
+
+}
+
+
+
---
+++ src/sigma2_epsilon_update.cpp
@@ -0,0 +1,31 @@
+#include "RcppArmadillo.h"
+#include "DLfuse.h"
+using namespace arma;
+using namespace Rcpp;
+
+// [[Rcpp::depends(RcppArmadillo)]]
+// [[Rcpp::export]]
+
+double sigma2_epsilon_update(arma::vec y,
+                             arma::vec sample_size,
+                             double alpha_sigma2_epsilon,
+                             double beta_sigma2_epsilon,
+                             arma::vec mean_temp){
+
+double alpha_sigma2_epsilon_update = 0.50*sum(sample_size) + 
+                                     alpha_sigma2_epsilon;
+
+double beta_sigma2_epsilon_update = 0.50*dot((y - mean_temp), (y - mean_temp)) + 
+                                    beta_sigma2_epsilon;
+
+double sigma2_epsilon = 1.00/R::rgamma(alpha_sigma2_epsilon_update,
+                                       (1.00/beta_sigma2_epsilon_update));
+
+return(sigma2_epsilon);
+
+}
+
+
+
+
+
